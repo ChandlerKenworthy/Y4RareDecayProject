376dc1f604754a4a05c85e4481ef5b2b20cc3eb8
Chandler Kenworthy <chandlerkenworthy@gmail.com>
2021-12-08 09:28:14 +0000

    Completed background models

--- background_fitting/bgfuncs.py
+++ background_fitting/bgfuncs.py
@@ -1,37 +1,28 @@
 import numpy as np
 
-def exponential(x, A, k, B, C):
-    """
-    A simple exponential function with a shift of B and amplitude A
-    """
-    return (A*np.exp(-((k*x)+B)))+C
+def exponential(m, a, b, c):
+    """ a*exp[-(bm+c)] """
+    return a * np.exp(-(b*m + c))
 
-def gaussian(x, mu, sigma):
-    """
-    A standard Gaussian distribution (not normalised)
-    """
-    return (1/(np.sqrt(2*np.pi)*sigma))*np.exp((x-mu)**2/(2*(sigma**2)))
+def expsquare(m, a, b, c):
+    """ defm """
+    return a*np.exp(-(m-b)**2)+c
 
-def power(x, A, B, C, D):
-    """
-    A simple power law function of the form A(x-B)^C
-    """
-    return A*(x-B)**(C) + D
+def gaussian(m, mu, sigma):
+    """ Standard normal distribution """
+    return (1/np.sqrt(2*np.pi*sigma**2))*np.exp(-((m-mu)**2)/(2*sigma**2))
 
-def logarithmic(x, A, B, C):
-    """
-    A logarithm-like function of the form f(x)=-Bln((x-A)) + C
-    """
-    return -(B * np.log(x-A)) + C
+def linear(m, a, b):
+    """ am + b """
+    return a*m + b
 
-def quadratic(x, b, c, d):
-    """
-    A polynomial function of the form ax^3 + bx^2 + cx + d
-    """
-    return b*(x**2) + (c*x) + d
+def quadratic(m, a, b, c):
+    """ am^2 + bm + c """
+    return a*(m**2) + (b*m) + c
 
-def linear(x, a, b):
-    """
-    The most trivial of functions y = ax +b
-    """
-    return a*x + b
+def cubic(m, a, b, c, d):
+    """ Standard cubic """
+    return a*(m**3) + b*(m**2) + (c*m) + d
+    
+    
+    
