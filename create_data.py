7d387866c4b755f74b92c9d67bb0537887800591
Chandler Kenworthy <chandlerkenworthy@gmail.com>
2022-03-10 17:21:33 +0000

    Finished create data and made requests ready

--- create_data.py
+++ create_data.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 import uproot as up
+import os
+from datetime import datetime, date
 
 # Utility functions
 def add_df_prefix(boolean_mask, prefix):
@@ -39,10 +41,11 @@ version = '7.0.0'
 preselection = True
 preselection_path = 'preselection.txt'
 random_seed = 0
+equalise_event_numbers = True
 
 # Open the file with all the user requested features, some may be expressions
-user_features = pd.read_csv('request.txt', index_col=None, header=None, sep=',')
-user_features.columns = ['Features']
+user_features = pd.read_csv('request.txt', index_col=None, sep=',')
+user_features['FeatureName'].fillna(user_features['Features'], inplace=True)
 
 # Find all the features that are custom expressions
 user_features['IsCustom'] = [' ' in f for f in user_features['Features']]
@@ -65,7 +68,7 @@ single_features = [f for f in request_features if type(f) is not list]
 multi_features = [f for f in request_features if type(f) is list]
 multi_features_flattened = [item for sublist in multi_features for item in sublist]
 # Ensure no features are requested multiple times (remove repeats)
-request_features = list(dict.fromkeys(single_features + multi_features_flattened))
+request_features = list(dict.fromkeys(single_features + multi_features_flattened + ['Lb_M']))
 
 # Get the features for the simulated data
 sim_request_features = request_features
@@ -85,8 +88,10 @@ with up.open(simulation_path + decay_tree_name) as f:
     sdf = sdf[~sdf.index.duplicated(keep='first')]
 
 if preselection:
+    print(f"Evaluating pre-selection for simulated data\nCurrently there are {len(sdf)} events")
     # Evaluate the pre-selection to remove events
     sdf = sdf[eval(sim_eval_ps)]
+    print(f"Preselection applied without error\nNow there are {len(sdf)} events\n")
     
 # Add a category column and a column to track simulated events
 sdf['category'] = np.where(sdf['Lb_BKGCAT'].isin([10, 50]), 1, 0)
@@ -110,8 +115,76 @@ with up.open(actual_path + decay_tree_name) as f:
     rdf = rdf[~rdf.index.duplicated(keep='first')]
 
 if preselection:
+    print(f"Evaluating pre-selection for real data\nCurrently there are {len(rdf)} events")
     # Evaluate the pre-selection to remove events
     rdf = rdf[eval(real_eval_ps)]     
-rdf['IsSimulation'] = False
+    print(f"Preselection applied without error\nNow there are {len(rdf)} events\n")
+rdf['IsSimulated'] = False
+rdf['category'] = 0
 
-print(sdf.head())
+# Remove the extra column that is in the simulated dataframe
+sdf.drop('Lb_BKGCAT', axis=1, inplace=True)
+
+# Join the dataframes together
+df = pd.concat([sdf, rdf], ignore_index=True, sort=False, axis=0)
+
+# Randomly shuffle the new dataframe
+df = df.sample(frac=1, random_state=random_seed)
+
+# Only keep the features we needed to fulfill the users request
+df = df[list(dict.fromkeys(request_features + ['category', 'Lb_M', 'IsSimulated']))]
+
+# Evaluate the custom expressions
+for index, row in user_features.iterrows():
+    # Does this row contain a custom feature
+    if row['IsCustom']:
+        df[row['FeatureName']] = eval(add_df_prefix(row['Features'], 'df')[0])
+    else:
+        pass
+    
+# Now we have made the custom features drop anything else not needed
+df = df[list(dict.fromkeys(user_features['FeatureName'].to_list() + ['Lb_M', 'IsSimulated', 'category']))]
+
+# Apply an event ratio restriction
+if equalise_event_numbers:
+    nbg = df['category'].value_counts()[0]
+    nsg = df['category'].value_counts()[1]
+    n_to_remove = np.abs(nsg - nbg)
+    
+    if nsg > nbg:
+        frac = n_to_remove / nsg
+        remove_idx = df.query('category == 1').sample(frac=frac, random_state=random_seed).index.to_list()
+        df.drop(remove_idx, axis=0, inplace=True)
+    elif nsg < nbg:
+        frac = n_to_remove / nbg
+        remove_idx = df.query('category == 0').sample(frac=frac, random_state=random_seed).index.to_list()
+        df.drop(remove_idx, axis=0, inplace=True)
+    else:
+        # They are already equal
+        pass
+    
+# Reset the index as well
+df.reset_index(drop=True, inplace=True)
+
+# Make a directory for all these data
+if not os.path.isdir(f'data_files/{version}'):
+    os.mkdir(f'data_files/{version}')
+df.to_csv(f'data_files/{version}/all.csv')
+
+# Output the metadata file as well
+print(f"Output to CSV: data_files/{version}/all.csv\nGenerating metadata file...")
+
+f = open(f'outputs/csv_metadata/{version}.txt', 'w')
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+today = date.today()
+d1 = today.strftime("%d/%m/%Y")
+f.write(f"Version: {version}\nCompile Date: {d1}\nCompile Time: {current_time}\nPreselection Applied: {preselection}\n")
+f.write(f"Equal Event Ratio: {equalise_event_numbers}\nFeatures Included:\n")
+
+for i, p in enumerate(user_features['Features'].to_list()):
+    string = f"{i}. {p}\n"
+    f.write(string)
+f.close()
+
+print(f"INFO: Metadata file generated at outputs/csv_metadata/{version}.txt'")
